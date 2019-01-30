a660623d42729e1403ca4080546fc0649f9a8c99
CNine <cnine229@gmail.com>
2019-01-30 16:49:26 +0800

    #build

---
+++ dist/mock-suit.js
@@ -0,0 +1,12 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global = global || self, global.MockSuit = factory());
+}(this, function () { 'use strict';
+
+	var MockSuit = 'MockSuit';
+
+	return MockSuit;
+
+}));
+//# sourceMappingURL=mock-suit.js.map
